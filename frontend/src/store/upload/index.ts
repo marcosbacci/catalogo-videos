f087126b0e81f0eaefc863504690df24ba86cb5f
Marcos Bacci <marcosfbacci@gmail.com>
2020-11-18 03:25:57 +0000

    Finalizando o reducer

--- frontend/src/store/upload/index.ts
+++ frontend/src/store/upload/index.ts
@@ -5,13 +5,19 @@ import update from "immutability-helper";
 export const {Types, Creators} = createActions <{
     ADD_UPLOAD: string,
     REMOVE_UPLOAD: string,
+    UPDATE_PROGRESS: string,
+    SET_UPLOAD_ERROR: string
 }, {
     addUpload(payload: Typings.AddUploadAction['payload']) : Typings.AddUploadAction,
     removeUpload(payload: Typings.RemoveUploadAction['payload']) : Typings.RemoveUploadAction,
+    updateProgress(payload: Typings.UpdateProgressAction['payload']) : Typings.UpdateProgressAction,
+    setUploadError(payload: Typings.SetUploadErrorAction['payload']) : Typings.SetUploadErrorAction
 }>
 ({
     addUpload: ['payload'],
-    removeUpload: ['payload']
+    removeUpload: ['payload'],
+    updateProgress: ['payload'],
+    setUploadError: ['payload']
 });
 
 export const INITIAL_STATE: Typings.State = {
@@ -20,7 +26,9 @@ export const INITIAL_STATE: Typings.State = {
 
 const reducer = createReducer<Typings.State, Typings.AddUploadAction>(INITIAL_STATE, {
     [Types.ADD_UPLOAD]: addUpload as any,
-    [Types.REMOVE_UPLOAD]: removeUpload as any
+    [Types.REMOVE_UPLOAD]: removeUpload as any,
+    [Types.UPDATE_PROGRESS]: updateProgress as any,
+    [Types.SET_UPLOAD_ERROR]: setUploadError as any
 });
 
 export default reducer;
@@ -67,6 +75,87 @@ function removeUpload(state: Typings.State = INITIAL_STATE, action: Typings.Remo
     }
 }
 
+function updateProgress(state: Typings.State = INITIAL_STATE, action: Typings.UpdateProgressAction) : Typings.State {
+    const videoId = action.payload.video.id;
+    const fileField = action.payload.fileField;
+    const {indexUpload, indexFile} = findIndexUploadFile(state, videoId, fileField);
+    if (typeof indexUpload === "undefined")
+        return state;
+
+    const upload = state.uploads[indexUpload];
+    const file = upload.files[indexFile];
+
+    if (file.progress === action.payload.progress)
+        return state;
+
+    const uploads = update(state.uploads, {
+        [indexUpload] : {
+            $apply(upload) {
+                const files = update(upload.files, {
+                    [indexFile] : {
+                        $set: {...file, progress: action.payload.progress}
+                    }
+                })
+                const progress = calculateGlobalProgress(files);
+                return {...upload, progress, files}
+            }
+        }
+    });
+
+    return {
+        uploads
+    };
+}
+
+function setUploadError(state: Typings.State = INITIAL_STATE, action: Typings.SetUploadErrorAction) : Typings.State {
+    const videoId = action.payload.video.id;
+    const fileField = action.payload.fileField;
+    const {indexUpload, indexFile} = findIndexUploadFile(state, videoId, fileField);
+    if (typeof indexUpload === "undefined")
+        return state;
+
+    const upload = state.uploads[indexUpload];
+    const file = upload.files[indexFile];
+
+    const uploads = update(state.uploads, {
+        [indexUpload] : {
+            files : {
+                [indexFile] : {
+                    $set: {...file, error: action.payload.error, progress: 1}
+                }
+            }
+        }
+    });
+
+    return {
+        uploads
+    };
+}
+
+function findIndexUploadFile(state:Typings.State, videoId, fileField) : {indexUpload?, indexFile?} {
+    const indexUpload = findIndexUpload(state, videoId);
+    if (indexUpload === -1)
+        return {}
+
+    const upload = state.uploads[indexUpload];
+    const indexFile = findIndexFile(upload.files, fileField);
+    return indexFile === -1 ? {} : {indexUpload, indexFile};
+}
+
+function calculateGlobalProgress(files: Array<{progress}>) {
+    const countFiles = files.length;
+    if (countFiles) {
+        return 0;
+    }
+
+    const sumProgress = files.reduce((sum, file) => sum + file.progress, 0);
+    return sumProgress / countFiles;
+}
+
 function findIndexUpload(state: Typings.State, id: string) {
     return state.uploads.findIndex((upload) => upload.video.id === id);
+}
+
+function findIndexFile(files: Array<{fileField}>, fileField: string) {
+    return files.findIndex((file) => file.fileField === fileField);
 }
